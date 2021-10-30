96fe8279a9985ea6c9103ac2b07fffe3c3e3379c
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2021-10-30 18:18:19 -0300

    hooks

---
+++ src/data/index.tsx
@@ -0,0 +1,5 @@
+export const allAnimes = [
+  { id: '16498', label: 'shingeki no kyojin'},
+  { id: '136', label: 'hunter x hunter'},
+  { id: '31964', label: 'boku no hero'},
+]
--- src/main/pages/Home/index.tsx
+++ src/main/pages/Home/index.tsx
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import { animes } from '@/services';
+
+import { ISearch, IAnimeResults } from '@/types';
+
 
 const Home: React.FC = () => {
+  const [anime, setAnime] = useState<IAnimeResults>({} as IAnimeResults);
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+
+  const { 
+    register, 
+    formState: { errors }, 
+    handleSubmit, 
+    getValues 
+  } = useForm<ISearch>();
+
+  const onSubmit: SubmitHandler<ISearch> = (data) => {
+    setIsLoad(true)
+    const { find } = data;
+    animes.get(`search/anime?q=${find}`)
+    .then( response => {
+      setAnime(response.data)
+    })
+    .catch( e => console.log(e))
+    .finally( () => {setIsLoad(false)})
+  };
+
+  console.log('anime ', anime)
+
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1>Escolha o seu anime</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="nome do anime"
+          {...register('find', { required: true })}
+        />
+        <span>{errors.find && 'informe o anime!'}</span>
+        <input
+          type="submit"
+          value="buscar"
+        />
+      </form>
+      <hr />
+      { isLoad ? (
+        <div>
+          <h1>Estamos pesquisando... aguarde instantes.</h1>
+        </div>
+      ) : (
+        <>
+        { anime?.results?.length <= 0 ? (
+          <h3>Por enquando, você ainda não pesquisou nada...</h3>
+        ) : (
+          <>
+            { anime?.results?.map( item => (
+              <div key={item.mal_id}>
+                <span>{item.title}</span>
+                <img src={item.image_url} alt={item.title} />
+                <p>{item.synopsis}</p>
+                <p><b>Nota: </b> {item.score}</p>
+              </div>
+            ) )}
+          </>
+        )}
+        </>
+      )}
     </div>
   );
 }
